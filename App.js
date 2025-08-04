7261999a4c7df91a2ab9af61e9a358a44f967cfe
Your Name <shivansh chaturvedi>
2025-08-04 09:57:46 +0530

    Writing first react code and  exploring cdn  react links

---
+++ App.js
@@ -0,0 +1,15 @@
+
+const heading=[React.createElement("div"
+,{id:"div"},
+React.createElement("div"
+,{id:"adiv"},"Another div"),[React.createElement("h1"
+,{id:"heading"},"This is heading"),React.createElement("h2"
+,{id:"heading2"},"This is heading2")]),React.createElement("div"
+,{id:"div"},
+React.createElement("div"
+,{id:"adiv2"},"Another div"),[React.createElement("h1"
+,{id:"heading"},"This is heading"),React.createElement("h2"
+,{id:"heading2"},"This is heading2")])]
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
